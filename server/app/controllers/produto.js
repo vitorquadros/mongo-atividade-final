9586018930b3f5df751a3b0282c4ab7197767e1f
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-11 14:31:27 -0300

    refact: formating and respond http erros

--- server/app/controllers/produto.js
+++ server/app/controllers/produto.js
@@ -1,33 +1,37 @@
 import * as model from '../models/produtosDao.js'
 
-const index = async (req,res) => {
+const index = async (req, res) => {
     let listProds
     console.log(req.query)
-    if(req.query.order){
+    if (req.query.order) {
         let orderBy = req.query.order
-        let reverse = req.query.reverse?true:false
-        listProds = await model.getAllProdutos(orderBy,reverse);
-    }else if(req.query.field && req.query.search){
-        listProds = await model.getFiltredProdutos(req.query.field,req.query.search);
-    }else{
+        let reverse = req.query.reverse ? true : false
+        listProds = await model.getAllProdutos(orderBy, reverse);
+    } else if (req.query.field && req.query.search) {
+        listProds = await model.getFiltredProdutos(req.query.field, req.query.search);
+    } else {
         listProds = await model.getAllProdutos();
     }
+    if (!listProds)
+        return res.status(404).send({ "message": "Sem resultados" })
     res.send(listProds);
 }
 
-const filterPrice = async (req,res) =>{
+const filterPrice = async (req, res) => {
     let status = 500;
-    try{
-        if(!req.query.greater|| !req.query.less){
+    try {
+        if (!req.query.greater || !req.query.less) {
             status = 401;
             throw new Error('Faltam parâmetros!')
         }
         const greater = Number(req.query.greater)
         const less = Number(req.query.less)
-        const sort = req.query.sort?req.query.sort:-1
-        const listProds = await model.getProdutosPriceRange(greater,less,sort)
+        const sort = req.query.sort ? req.query.sort : -1
+        const listProds = await model.getProdutosPriceRange(greater, less, sort)
+        if (!listProds)
+            return res.status(404).send({ "message": "Sem resultados" })
         res.send(listProds)
-    }catch(error){
+    } catch (error) {
         const mensagem = { erro: error }
         console.log(mensagem)
         res.status(status);
@@ -47,20 +51,20 @@ const store = async (req, res) => {
     try {
         const formData = req.body;
         console.log(formData, req.body)
-        console.log(formData, {...req.body})
+        console.log(formData, { ...req.body })
         const produto = {
-            id_prod:  +formData.id_prod,
+            id_prod: +formData.id_prod,
             nome: formData.nome,
             descricao: formData.descricao,
-            qtd_estoque:  +formData.qtd_estoque,
-            preco:  +formData.preco,
+            qtd_estoque: +formData.qtd_estoque,
+            preco: +formData.preco,
             importado: formData.importado ? true : false,
-            desconto: formData.desconto? +formData.desconto:null
+            desconto: formData.desconto ? +formData.desconto : null
         }
 
 
-        if(!produto.preco && formData.preco.indexOf(','))
-            produto.preco = +formData.preco.replace(',','.')
+        if (!produto.preco && formData.preco.indexOf(','))
+            produto.preco = +formData.preco.replace(',', '.')
 
         if (await model.insertProduto(produto))
             res.send({
@@ -87,15 +91,15 @@ const update = async (req, res) => {
         const newProduto = {}
         newProduto.id_prod = +req.params.id
         formData.qtd_estoque && (newProduto.qtd_estoque = +formData.qtd_estoque)
-        
-        if(formData.preco && formData.preco.indexOf(',')){
-            formData.preco=formData.preco.replace(',','.')
+
+        if (formData.preco && formData.preco.indexOf(',')) {
+            formData.preco = formData.preco.replace(',', '.')
             newProduto.preco = +formData.preco
         }
 
         formData.importado && (newProduto.importado = true)
         formData.desconto && (newProduto.desconto = +formData.desconto)
-        
+
         if (!(await model.updateProduto(newProduto)))
             throw new Error("Erro ao atualizar produto!")
         res.send({
